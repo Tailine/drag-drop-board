508400a14a5f66510ba0926eb14f91114bf7a249
Tailine <tailinechagas@gmail.com>
2023-05-14 19:35:51 -0300

    refactor: rename component

---
+++ src/components/CardBoard/CardBoard.styles.ts
@@ -0,0 +1,6 @@
+import { styled } from '@/config'
+
+export const Paragraph = styled('p', {
+  color: '$gray',
+  fontSize: '$md'
+})
--- src/components/CardBoard/CardBoard.tsx
+++ src/components/CardBoard/CardBoard.tsx
@@ -1,31 +1,28 @@
 import { BoardItem } from '@/App'
-import * as S from './Item.styles'
+import * as S from './CardBoard.styles'
 import { DragEvent } from 'react'
 import { useRef } from 'react'
+import { Card } from '@/components/Card'
 
 type Props = {
   data: BoardItem
 }
 
-export function Item({ data }: Props) {
+export function CardBoard({ data }: Props) {
   const dragItem = useRef<HTMLDivElement>(null)
 
   function handleDragStart(e: DragEvent<HTMLDivElement>) {
     e.dataTransfer.setData('text/plain', JSON.stringify(data))
   }
 
-  // function handleDrop(e: DragEvent<HTMLDivElement>) {
-  //   e.
-  // }
-
   return (
-    <S.Wrapper
+    <Card
       ref={dragItem}
       draggable
       onDragStart={handleDragStart}
       onDragEnd={(e) => console.log('End', e)}
     >
-      {data.value}
-    </S.Wrapper>
+      <S.Paragraph>{data.value}</S.Paragraph>
+    </Card>
   )
 }
